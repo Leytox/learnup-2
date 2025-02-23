25a3d57f176c25adbfbc73d648c29e23537b76cd
Ilya Devder <iladevder@gmail.com>
2025-02-23 22:57:12 +0200

    initial profile page setup

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,3 +1,17 @@
-export default function Profile() {
-  return <main className="min-h-screen"></main>;
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function Profile() {
+  const session = await auth();
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="container">
+        <div className="relative w-20 h-20 rounded-full overflow-hidden">
+          <Image src={session?.user?.image || ""} fill alt="user's avatar" />
+        </div>
+        <h1 className="text-2xl font-semibold">Profile</h1>
+        <p className="text-gray-500">This is your profile page.</p>
+      </div>
+    </main>
+  );
 }
