ea5a9486f29d987d8b42f92f4cc45f75cd089788
Ilya Devder <iladevder@gmail.com>
2025-03-16 22:46:38 +0200

    Update logo text gradient colors

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-
 export default function Logo() {
   return (
     <h1
       className={
-        "bg-gradient-to-b from-white via-gray-300 to-gray-400 bg-clip-text text-transparent text-3xl tracking-normal font-semibold pointer-events-none"
+        "bg-gradient-to-b from-gray-500 via-gray-400 to-gray-600 bg-clip-text text-transparent text-3xl tracking-normal font-semibold pointer-events-none"
       }
     >
       Learn
       <span
         className={
-          "bg-gradient-to-r from-green-500 via-green-600 to-green-700 bg-clip-text text-transparent"
+          "bg-gradient-to-r from-green-500 via-green-600 to-green-800 bg-clip-text text-transparent"
         }
       >
         Up
