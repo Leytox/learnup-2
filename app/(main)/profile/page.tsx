0d246f41e93129c797e8f4b81bf7503c58803672
Ilya Devder <iladevder@gmail.com>
2025-03-16 22:42:41 +0200

    Add default avatar and display user name in profile page
    Add default avatar and display user name in profile page

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -7,9 +7,13 @@ export default async function Profile() {
     <main className="min-h-screen flex flex-col items-center">
       <div className="container">
         <div className="relative w-20 h-20 rounded-full overflow-hidden">
-          <Image src={session?.user?.image || ""} fill alt="user's avatar" />
+          <Image
+            src={session?.user?.image || "/default.jpg"}
+            fill
+            alt="user's avatar"
+          />
         </div>
-        <h1 className="text-2xl font-semibold">Profile</h1>
+        <h1 className="text-2xl font-semibold">{session?.user?.name}</h1>
         <p className="text-gray-500">This is your profile page.</p>
       </div>
     </main>
