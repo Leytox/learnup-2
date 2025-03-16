160eb2634099cd61f746262f3d407b79be5f6908
Ilya Devder <iladevder@gmail.com>
2025-03-16 22:41:37 +0200

    Restructure auth pages and add registration component

---
+++ app/(main)/(auth)/registration/page.tsx
@@ -0,0 +1,11 @@
+import { RegistrationForm } from "@/components/registration-form";
+
+export default function Login() {
+  return (
+    <div className="min-h-svh flex items-center justify-center w-full p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegistrationForm />
+      </div>
+    </div>
+  );
+}
