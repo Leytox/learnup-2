a56ffe3f8e2575359985ed30f1fadfb40d62e45f
Ilya Devder <iladevder@gmail.com>
2025-07-05 17:30:27 +0300

    improved search ui/ux

--- components/search.tsx
+++ components/search.tsx
@@ -6,6 +6,7 @@ import { Input } from "./ui/input";
 
 export default function SearchButton() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     inputRef.current?.focus();
@@ -22,23 +23,45 @@ export default function SearchButton() {
         <Search />
       </Button>
       {isModalOpen && (
-        <div className="h-screen w-screen fixed backdrop-blur-md bg-transparent top-0 left-0">
+        <div className="h-screen w-screen fixed bg-transparent animate-blur top-0 left-0">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+            className="absolute top-2 right-2"
+          >
+            <X />
+          </Button>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-4">
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => setIsModalOpen(false)}
-            >
-              <X />
-            </Button>
-            <Input placeholder="Search..." className="w-96" ref={inputRef} />
-            <Button
-              variant="ghost"
-              size="icon"
-              onClick={() => console.log("Search clicked")}
-            >
-              <Search />
-            </Button>
+            <form>
+              <div className="relative">
+                <Search
+                  className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400"
+                  size={16}
+                />
+                <Input
+                  type="search"
+                  placeholder="Search..."
+                  className="w-96 pl-8"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  ref={inputRef}
+                />
+                {searchTerm.length > 0 && (
+                  <Button
+                    variant="link"
+                    size="icon"
+                    onClick={() => setSearchTerm("")}
+                    className="absolute right-1 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-500"
+                    title="Clear search"
+                  >
+                    <X />
+                  </Button>
+                )}
+              </div>
+            </form>
           </div>
         </div>
       )}
